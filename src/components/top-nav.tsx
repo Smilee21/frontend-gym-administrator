4cc29d5a0a62707a4e68f6c803489d640a463e83
Smilee21 <antomigel23@gmail.com>
2024-10-02 23:12:00 -0400

    initial calendar

---
+++ src/components/top-nav.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react'
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+  navigationMenuTriggerStyle,
+} from '@/components/ui/navigation-menu'
+import Link from 'next/link'
+
+export default function TopNavigation() {
+  return (
+    <header className="w-full h-24 z-50  bg-white bg-opacity-15 m-0 backdrop-blur-lg border shadow-md flex justify-between fixed px-32">
+      <h2 className="self-center">FIT101</h2>
+
+      <NavigationMenu className=" self-center ">
+        <NavigationMenuList className="flex gap-8">
+          <NavigationMenuItem>
+            <Link href="/" legacyBehavior passHref prefetch={false}>
+              <NavigationMenuLink className="">home</NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/about" legacyBehavior passHref prefetch={false}>
+              <NavigationMenuLink className="">about us</NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/trainers" legacyBehavior passHref prefetch={false}>
+              <NavigationMenuLink className="">trainers</NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </header>
+  )
+}
