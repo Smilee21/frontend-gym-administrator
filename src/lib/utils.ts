5265af0fa9db9a6a906032280cfc67ec2d8db65c
Smilee21 <antomigel23@gmail.com>
2024-10-06 15:12:00 -0400

    create a ultil for format hour from db

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,15 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+import dayjs from 'dayjs'
+import customParseFormat from 'dayjs/plugin/customParseFormat'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+dayjs.extend(customParseFormat)
+
+export const formatHour = (time: string) => {
+  // Indicar el formato de entrada `HH:mm:ss` (24 horas)
+  return dayjs(time, 'HH:mm:ss').format('hh:mm A')
 }
