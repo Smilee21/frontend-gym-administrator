0dcfafe8a62a6a722e0f8d755d6f0a16464d6192
Smilee21 <antomigel23@gmail.com>
2024-10-06 15:12:54 -0400

    modify structure component for show just a column by day

--- src/components/calendar/day.tsx
+++ src/components/calendar/day.tsx
@@ -1,6 +1,7 @@
 import './calendar.css'
 import { ITrainingSessions, ITrainer } from '@/interfaces/training-sessions'
 import React, { useEffect, useState } from 'react'
+import { formatHour } from '@/lib/utils'
 
 export default function DayColumn() {
   const [data, setData] = useState<ITrainingSessions[]>([])
@@ -8,9 +9,8 @@ export default function DayColumn() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('http://localhost:3000/training-session/')
+        const response = await fetch('http://localhost:3000/training-session')
         const result: ITrainingSessions[] = await response.json()
-        console.log(result)
         setData(result)
       } catch (error) {
         console.error('Error fetching data:', error)
@@ -24,30 +24,43 @@ export default function DayColumn() {
     console.log(id)
   }
 
+  type DaysGrouped = { [key: string]: ITrainingSessions[] }
+
+  // Agrupar las sesiones por día
+  const daysGrouped: DaysGrouped = data.reduce((acc: DaysGrouped, info) => {
+    acc[info.day] = acc[info.day] ? [...acc[info.day], info] : [info]
+    return acc
+  }, {})
+
+  // Renderizar
   return (
     <div className="day-container-column">
-      {data.map((el) => (
-        <section className="day-column" key={el.id}>
+      {Object.entries(daysGrouped).map(([day, classes]) => (
+        <section className="day-column" key={day}>
           <header className="flex justify-center">
-            <h3 className="">{el.day}</h3>
+            <h3>{day}</h3>
           </header>
-
-          <article
-            className={el.spaces > 0 ? 'card-day' : 'card-day full-day-card'}
-            onClick={() => handleClick(el.id)}
-          >
-            <div className="z-40 relative flex flex-col items-center justify-center gap-10">
-              <header className="top-[-15px] relative">
-                <h3>{el.hour}</h3>
-              </header>
-              <section className="flex flex-col items-center  gap-2">
-                <p>{el.trainer?.name || 'Not Asigned'}</p>
-                <span>
-                  {el.spaces > 0 ? 'Available:' + el.spaces : 'FULL DAY'}
-                </span>
-              </section>
-            </div>
-          </article>
+          {classes.map((info) => (
+            <article
+              className={
+                info.spaces > 0 ? 'card-day' : 'card-day full-day-card'
+              }
+              onClick={() => handleClick(info.id)}
+              key={info.id}
+            >
+              <div className="z-100 relative flex flex-col items-center justify-center gap-10">
+                <header className="top-[-15px] relative">
+                  <h3>{formatHour(info.hour)}</h3>
+                </header>
+                <section className="flex flex-col items-center gap-2">
+                  <p>{info.trainer?.name || 'Not Assigned'}</p>
+                  <span>
+                    {info.spaces > 0 ? 'Available: ' + info.spaces : 'FULL DAY'}
+                  </span>
+                </section>
+              </div>
+            </article>
+          ))}
         </section>
       ))}
     </div>
