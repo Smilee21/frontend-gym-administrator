6d854de5a0facde747bd56a915348929c54c021e
Smilee21 <antomigel23@gmail.com>
2024-10-09 12:00:51 -0400

    change names of env vars

--- src/config/amplify-auth.ts
+++ src/config/amplify-auth.ts
@@ -3,9 +3,9 @@ import { Amplify } from 'aws-amplify'
 Amplify.configure({
   Auth: {
     Cognito: {
-      userPoolId: `${process.env.NEXT_PUBLIC_AWS_USER_POOL_ID}`,
-      userPoolClientId: `${process.env.NEXT_PUBLIC_AWS_COGNITO_CLIENT_ID}`,
-      identityPoolId: `${process.env.NEXT_PUBLIC_AWS_IDENTITY_POOL_ID}`,
+      userPoolId: `${process.env.NEXT_PUBLIC_USER_POOL_ID}`,
+      userPoolClientId: `${process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID}`,
+      identityPoolId: `${process.env.NEXT_PUBLIC_IDENTITY_POOL_ID}`,
 
       loginWith: {
         email: true,
